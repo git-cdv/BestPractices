ef95d0aaf2fb5a75eace76e3406d4f962e46a765
chkan <chkan.dv@gmail.com>
2022-08-31 09:53:04 +0300

    MVI example

--- app/src/main/java/com/chkan/bestpractices/ui/main/MenuFragment.kt
+++ app/src/main/java/com/chkan/bestpractices/ui/main/MenuFragment.kt
@@ -1,31 +1,26 @@
 package com.chkan.bestpractices.ui.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.chkan.bestpractices.R
+import com.chkan.bestpractices.core.BaseFragment
 import com.chkan.bestpractices.core.extensions.activityNavController
 import com.chkan.bestpractices.core.extensions.navigateSafely
-import com.google.android.material.button.MaterialButton
+import com.chkan.bestpractices.databinding.FragmentMenuBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MenuFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_menu, container, false)
-    }
+class MenuFragment : BaseFragment<FragmentMenuBinding>(FragmentMenuBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<MaterialButton>(R.id.btn_simple_paging).setOnClickListener {
+        binding.btnSimplePaging.setOnClickListener {
             activityNavController().navigateSafely(R.id.action_menuFragment_to_SimplePagingFlowFragment)
         }
+        binding.btnMvi.setOnClickListener {
+            activityNavController().navigateSafely(R.id.action_menuFragment_to_mviListFragment)
+        }
+
     }
 
 }
--- app/src/main/java/com/chkan/bestpractices/ui/mvi_example/ui/MviListFragment.kt
+++ app/src/main/java/com/chkan/bestpractices/ui/mvi_example/ui/MviListFragment.kt
@@ -5,16 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.chkan.bestpractices.R
+import com.chkan.bestpractices.core.BaseFragment
+import com.chkan.bestpractices.databinding.FragmentMviListBinding
+import com.chkan.bestpractices.ui.mvi_example.model.LceGesture
+import com.chkan.bestpractices.ui.mvi_example.model.LceUiState
+import com.chkan.bestpractices.ui.mvi_example.model.LceViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-class MviListFragment : Fragment() {
+@AndroidEntryPoint
+class MviListFragment : BaseFragment<FragmentMviListBinding>(FragmentMviListBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mvi_list, container, false)
+    private val viewModel: LceViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.state.collect {
+                when(it){
+                    is LceUiState.ItemList -> {
+                        binding.btn1.apply {
+                            isVisible = true
+                            text = it.items[0].name
+                            setOnClickListener { view ->
+                                viewModel.process(LceGesture.ItemClicked(it.items[0].id))
+                            }
+                        }
+                        binding.btn2.apply {
+                            isVisible = true
+                            text = it.items[1].name
+                            setOnClickListener { view ->
+                                viewModel.process(LceGesture.ItemClicked(it.items[1].id))
+                            }
+                        }
+                        binding.bar.isVisible = false
+                    }
+                    is LceUiState.Loading -> {
+                        binding.btn1.isVisible = false
+                        binding.btn2.isVisible = false
+                        binding.bar.isVisible = true
+                    }
+                    is LceUiState.Item -> {
+                        binding.btn1.isVisible = false
+                        binding.btn2.isVisible = false
+                        binding.textView.text = it.contents
+                        binding.bar.isVisible = false
+                    }
+                    is LceUiState.Error -> {
+                        binding.btn1.isVisible = false
+                        binding.btn2.isVisible = false
+                        binding.textView.text = "ERROR"
+                        binding.bar.isVisible = false
+                    }
+                }
+
+            }
+        }
+
     }
 
 }
